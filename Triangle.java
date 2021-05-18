188f1c1d94858765be759c6229824071d1f6b7ca
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-05-19 00:49:32 +0530

    hackerrank solution
    java solution of Small Triangles, Big Triangles solution from Hackerrank

---
+++ Triangle.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Triangle {
+    
+    static HashMap<Double, ArrayList<Double>> hash = new HashMap<>();
+    public static void sortbykey()
+    {
+        TreeMap<Double, ArrayList<Double>> sorted = new TreeMap<>();
+ 
+        sorted.putAll(hash);
+        for (HashMap.Entry<Double, ArrayList<Double>> entry : sorted.entrySet())
+            System.out.println(entry.getValue());       
+    }
+
+    public static void main(String[] args) {
+        double t = 0;
+        Scanner sc = new Scanner(System.in);
+        t = sc.nextDouble();
+        while(t-->0){
+            double a , b, c, p, s, eqn;
+            ArrayList<Double> sides = new ArrayList<>();
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            c = sc.nextDouble();
+            sides.add(a);
+            sides.add(b);
+            sides.add(c);
+
+            p = (a+b+c)/2;
+            eqn = p*(p-a)*(p-b)*(p-c);
+            s = Math.sqrt(eqn);
+            hash.put(s,sides);
+        }
+        sortbykey();
+        sc.close();
+    }
+}
