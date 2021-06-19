de338d5c0ce8bb4dfa21b93e3da3da627010652b
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-06-20 02:11:04 +0530

    1030A

---
+++ Array.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n; cin >> n;
+    int arr[n];
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+    if(sum != 0){
+        cout << "HARD\n";
+    } else {
+        cout << "EASY\n";
+    }
+    return 0;
+}
